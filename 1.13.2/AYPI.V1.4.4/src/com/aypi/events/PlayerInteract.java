87a9bfcc9e100f6d371367863f56f3e341b19169
AymegikeLewok <aymeric.henouille@gmail.com>
2018-12-04 20:14:32 +0100

    AYPIV1.4.4: zone prority update

---
+++ 1.13.2/AYPI.V1.4.4/src/com/aypi/events/PlayerInteract.java
@@ -0,0 +1,33 @@
+package com.aypi.events;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import com.aypi.Aypi;
+import com.aypi.utils.Zone;
+import com.aypi.utils.ZonePriorityBuffer;
+
+public class PlayerInteract implements Listener {
+	
+	@EventHandler
+	public void onPlayerClick(PlayerInteractEvent e) {
+		Player player = e.getPlayer();
+
+		Location loc = player.getLocation();
+		ZonePriorityBuffer zpb = new ZonePriorityBuffer();
+		
+		for (Zone zone : Aypi.getZoneManager().getZones()) {
+			if (zone.containLocation(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()))) {
+				zpb.addZone(zone);
+			}
+		}
+		
+		for (Zone zone : zpb.getPriorityZones()) {
+			zone.getZoneListener().onPlayerTryToInteractEvent(player, e);
+		}
+	}
+
+}
